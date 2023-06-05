720bd4591e65747831f247695629e73569e0b67c
Emilia <yourbelovedemilia@yandex.ru>
2023-06-05 22:45:14 +0300

    Экран профиля пользователя, свёрстанный кодом

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Эмилия on 30.05.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    private func initAvatarImage(view: UIView) {
+        view.backgroundColor = UIColor(named: "ypBlack")
+        
+        let avatarImage = UIImage(named: "avatar")
+        let avatarImageView = UIImageView(image: avatarImage)
+        avatarImageView.tag = 1
+        
+        avatarImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(avatarImageView)
+        avatarImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        avatarImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        avatarImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        avatarImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
+    }
+    
+    private func initLogoutButton(view: UIView) {
+        let logoutButton = UIButton.systemButton(
+                    with: UIImage(named: "logout_button")!,
+                    target: self,
+                    action: #selector(Self.didTapLogoutButton)
+                    )
+        
+        logoutButton.tintColor = UIColor(named: "ypRed")
+        
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoutButton)
+        logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20).isActive = true
+        logoutButton.centerYAnchor.constraint(equalTo: view.viewWithTag(1)!.centerYAnchor).isActive = true
+        
+    }
+    
+    private func initLabels(view: UIView) {
+        let nameLabel = UILabel()
+        nameLabel.text = "Екатерина Новикова"
+        nameLabel.textColor = UIColor(named: "ypWhite")
+        nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        nameLabel.topAnchor.constraint(equalTo: view.viewWithTag(1)!.bottomAnchor, constant: 8).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: view.viewWithTag(1)!.leadingAnchor).isActive = true
+        
+        let loginNameLabel = UILabel()
+        loginNameLabel.text = "@ekaterina_nov"
+        loginNameLabel.textColor = UIColor(named: "ypGray")
+        loginNameLabel.font = UIFont.systemFont(ofSize: 13)
+        
+        loginNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginNameLabel)
+        loginNameLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor).isActive = true
+        loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        loginNameLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "Hello, world!"
+        descriptionLabel.textColor = UIColor(named: "ypWhite")
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13)
+        
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        descriptionLabel.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8).isActive = true
+        descriptionLabel.leadingAnchor.constraint(equalTo: nameLabel.leadingAnchor).isActive = true
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initAvatarImage(view: view)
+        initLogoutButton(view: view)
+        initLabels(view: view)
+    }
+    
+    @objc
+    private func didTapLogoutButton() {
+        for view in view.subviews {
+            if view is UILabel {
+                view.removeFromSuperview()
+            } else {
+                if let imageView = view as? UIImageView {
+                    imageView.image = UIImage(named: "avatar_none")
+                }
+            }
+        }
+    }
+}
+
+extension ProfileViewController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
